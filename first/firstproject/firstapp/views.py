668f8667bfbd209f86fc004351ee736badf37ac6
tanimtbh <tanimtbh@gmail.com>
2021-04-05 17:04:49 +0200

    django mongodb atlas connection

--- first/firstproject/firstapp/views.py
+++ first/firstproject/firstapp/views.py
@@ -7,5 +7,5 @@ def index(request):
     # now = datetime.datetime.now()
     # html = "<html><body>It is now %s.</body></html>" % now
     # return HttpResponse(html)
-    my_dic={'insert_me' : "This is from firstApp view"}
+    my_dic={'insert_me' : "This is from firstApp view add donw by abba tanim"}
     return render(request,"firstapp/index.html",context=my_dic)
--- first/firstproject/firstproject/settings.py
+++ first/firstproject/firstproject/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import urllib
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -78,12 +79,14 @@ WSGI_APPLICATION = 'firstproject.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'default': {
+        'ENGINE': 'djongo',
+        'NAME': 'tanim',
+        'HOST': 'mongodb+srv://tanim:abbatanim@clouddb.kzvmd.mongodb.net/testdb?retryWrites=true&w=majority',
+        'USER': 'tanim',
+        'PASSWORD': 'abbatanim',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
